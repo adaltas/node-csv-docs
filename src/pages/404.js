179e0e6024ba61cf41ea02bb77ae3f2e66f20ae3
David Worms <david@adaltas.com>
2018-09-24 14:19:11 +0200

    404: inject page information

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,13 @@ import React from 'react'
 import Layout from '../layout'
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout
+    page={{
+      title: 'Page not found',
+      description: 'The requested page does not exist',
+      keywords: 'csv, node.js, 404, not found'
+    }}
+  >
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
